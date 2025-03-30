86718436e9e9eb758b990b68e7844829a7ea3a58
reza <khoeinireza8@gmail.com>
2025-03-30 17:51:29 +0330

    delete some texts

--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -20,9 +20,6 @@ public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
 
-        System.out.println("hello temp");
-        System.out.println("hello new temp");
-
     }
 
 }
