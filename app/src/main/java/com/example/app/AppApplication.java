d0d77f9393c53900ed07c41bace0090391c63574
reza <khoeinireza8@gmail.com>
2025-03-30 16:37:01 +0330

    add hello temp

--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -19,6 +19,7 @@ public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+        System.out.println("hello temp");
     }
 
 }
